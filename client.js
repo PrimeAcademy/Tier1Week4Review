608576b520a227e656d6a6198efd66e6f742a3ab
Dev Jana <dev@primeacademy.io>
2018-06-13 14:14:15 -0500

    solution finished

--- client.js
+++ client.js
@@ -1,1 +1,11 @@
 var myArray = [ 12, 1, 154, 13, 9, 234, 2, 3.1415, 434, 612 ];
+
+function getBigger( min, array ){
+  var bigOnes = [];
+  for( number of myArray ){
+    if( number > min ){
+      bigOnes.push( number )
+    } //end if
+  }
+  return bigOnes;
+}
